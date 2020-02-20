831e29acff3a1351c8bbcc7d3926925795d24a94
Rye Carrigan <rcarrigan@cloudbees.com>
2020-02-20 13:23:36 -0500

    Adding my production key :/

--- sko.py
+++ sko.py
@@ -19,7 +19,7 @@ flags = Flags()
 Rox.register("default", flags)
 
 # Setup the Rollout environment key
-cancel_event = Rox.setup("<rollout_env_key>").result();
+cancel_event = Rox.setup("5e4ecd8fff09bf65fd0ddeee").result();
 
 # Boolean flag example
 print('enableTutorial is %s' % flags.enableTutorial.is_enabled())
